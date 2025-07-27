a1b140dc872ee3636a9326c56d22046db87d0f81
hvhi56 <3011760@GMAIL.COM>
2025-07-27 21:12:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from telethon import TelegramClient, events
 import requests
 
-# פרטי חשבון הטלגרם שלך
 api_id = 25863606
 api_hash = '34f981178528c8167680f1429bb526c6'
 
 # טוקן של הבוט שלך
 bot_token = '7723230187:AAFNSF3UusSY81rq5d9qrZDxhSTcwlgEams'
-chat_id = '@your_bot_channel_or_chat'  # נעדכן בהמשך
 
-# הערוץ שמהואזינים לו
+# כאן תשלח את ההודעות – משתמש ששלח לבוט קודם
+chat_id = 6708985488
+
 source_channel = 'https://t.me/Moshepargod'
 
 client = TelegramClient('session', api_id, api_hash)
 
 @client.on(events.NewMessage(chats=source_channel))
 async def handler(event):
+    if chat_id is None:
+        return
     message_text = event.raw_text
     if message_text.strip() == "":
         return
